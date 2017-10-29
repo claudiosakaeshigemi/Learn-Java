ea129651f409ce50937fbb30055d4e65f81655dd
dimsol <claudiosakaeshigemi@gmail.com>
2017-10-29 10:13:59 -0200

    Criação do projeto Aprendendo Java.

---
+++ app/src/main/java/com/claudioshigemi/learnjava/HellorWorld.java
@@ -0,0 +1,14 @@
+package com.claudioshigemi.learnjava;
+
+/**
+ * Created by Claudio on 29/10/2017.
+ */
+
+public class HellorWorld {
+    //psvm
+    public static void main(String[] args) {
+        //sop
+        System.out.println("Hello World.");
+    }
+
+}
